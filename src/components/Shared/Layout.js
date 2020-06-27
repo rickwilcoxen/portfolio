ddcf177e1a5a57873369498c6a68c67ec135d4f9
rickwilcoxen <rickwilcoxen95@gmail.com>
2020-06-27 16:38:14 -0400

    Changed color of buttons added Layout
    Created layout component for shared folder
    Added other colors to SolidButton

---
+++ src/components/Shared/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Layout = (props) => (
+  <div className='DivDesign'>
+    {props.children}
+  </div>
+)
+export default Layout
--- src/components/Shared/SolidButton.js
+++ src/components/Shared/SolidButton.js
@@ -2,16 +2,13 @@ import styled from 'styled-components'
 
 const SolidButton = styled.button`
 border-radius: 20px;
-border-width: 2px;
-background-color: ${props => props.primaryColor};
-color: darkgray;
+background-color: #FFF6A7;
 font-size: 1.2rem;
 padding: 10px 40px;
 margin: 5px;
 transition: color .2s linear, background-color .2s linear;
 &:hover {
-  background-color: gray;
-  color: ${props => props.primaryColor};
+  background-color: #F8C471;
 }
 `
 
